18b25f204c295015d1461399f829280a4a8fada0
Juan Hinojo <jchinojob7@gmail.com>
2023-01-31 13:21:23 -0600

    Add commands fish file

---
+++ home/.config/fish/functions/commands.fish
@@ -0,0 +1,6 @@
+function ga; git add .; end
+function gc; git commit -m $argv; end
+function gp; git push $argv; end
+function v; nvim $argv; end
+function cat; bat $argv; end
+function django; python manage.py $argv; end
