4b142910aa1fbf918f0f2454f6067147ca3aad61
Juan Hinojo <jchinojob7@gmail.com>
2024-07-10 16:20:04 -0500

    Extra octo nvim mappings

--- home/.config/nvim/lua/keymaps.lua
+++ home/.config/nvim/lua/keymaps.lua
@@ -20,9 +20,10 @@ map("md", ":lua require('dap').step_over()<CR>", "silent")
 map("mi", ":lua require('dap').step_into()<CR>", "silent")
 
 -- Octo mappings
-map("<Leader>op", ":Octo pr list<CR>", "silent")
+map("<Leader>op", ":Octo pr ")
+map("<Leader>ol", ":Octo pr list<CR>", "silent")
 map("<Leader>on", ":Octo pr create<CR>", "silent")
-map("<Leader>od", ":Octo pr diff<CR>", "silent")
+map("<Leader>ov", ":Octo pr checks<CR>", "silent")
 map("<Leader>os", ":Octo review submit<CR>", "silent")
 
 -- Telescope
