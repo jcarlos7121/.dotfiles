55144c3ad0b007f8e1540d8c5a4c7def0ed7720e
Juan Hinojo <jchinojob7@gmail.com>
2025-01-23 23:19:12 -0600

    Remove more apps from Yabai to be managed

--- home/.config/nvim/after/plugin/lsp-server-defaults.lua
+++ home/.config/nvim/after/plugin/lsp-server-defaults.lua
@@ -6,12 +6,12 @@ local on_attach = function(client, bufnr)
 
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_command [[augroup Format]]
-    vim.api.nvim_command [[autocmd! * <buffer>]]
-    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-    vim.api.nvim_command [[augroup END]]
-  end
+  -- if client.server_capabilities.documentFormattingProvider then
+  --   vim.api.nvim_command [[augroup Format]]
+  --   vim.api.nvim_command [[autocmd! * <buffer>]]
+  --   vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+  --   vim.api.nvim_command [[augroup END]]
+  -- end
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities(
