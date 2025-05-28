db441686380188c9962f371e973c2d31ae30411a
Juan Hinojo <jchinojob7@gmail.com>
2025-05-28 12:46:45 -0500

    Modify nvim tree sitter subjects and objects configuration

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -89,21 +89,11 @@ return {
           enable = true,
           set_jumps = true,
           goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = '@class.outer'
-          },
-          goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer'
+            [']]'] = '@function.outer'
           },
           goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer'
+            ['[['] = '@function.outer'
           },
-          goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer'
-          }
         }
       }
     }
